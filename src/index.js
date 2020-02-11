ddc25aa3b343d4d14a0890a3577e4cd047c22be8
SteVasc <stefany.vasc.sa@gmail.com>
2020-02-11 10:48:09 -0300

    :construction: challenge 021 done
    included card reveal with icon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const $root = document.querySelector("#root");
 
 /* criando o article */
 const $memoryCard = document.createElement("article");
+const $memoryCardReveal = document.createElement("article");
 
 /* outra forma de adicionar elementos diretamente */
 
@@ -15,11 +16,17 @@ const $icon = `
   />
 `;
 
+const $iconReveal = `<img class="icon -ball" src="img/icon-c.png" alt="icone c" />`;
+
 /* adicionando a classe para poder ganhar o estilo do css */
 $memoryCard.classList.add("memory-card");
+$memoryCardReveal.classList.add("memory-card", "-reveal");
 
 /* colocando o elemento article dentro do root
 
 insertBefore(elemento que quero IntersectionObserver, antes de qual elemento) */
 $root.insertBefore($memoryCard, null);
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$root.insertBefore($memoryCardReveal, $memoryCard);
+$memoryCardReveal.insertAdjacentHTML("afterbegin", $iconReveal);
