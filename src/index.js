4d84aa353040cd3a0e5a4593666770c2ea09b34c
Stefany Sá <stefany.vasc.sa@gmail.com>
2020-02-16 23:27:35 -0300

    :construction: challenge basic 022 done
    replicate the cards as in the mobile layout

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
 const $memoryCardFront = document.createElement("article");
 
+const $memoryCard2 = document.createElement("article");
+const $memoryCard3 = document.createElement("article");
+const $memoryCard4 = document.createElement("article");
+const $memoryCard5 = document.createElement("article");
+const $memoryCard6 = document.createElement("article");
+const $memoryCard7 = document.createElement("article");
+const $memoryCard8 = document.createElement("article");
+const $memoryCard9 = document.createElement("article");
+const $memoryCard10 = document.createElement("article");
+const $memoryCard11 = document.createElement("article");
 /* outra forma de adicionar elementos diretamente */
 
 const $iconCollab = `
@@ -34,3 +44,53 @@ $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
 
 $root.insertBefore($memoryCardFront, $memoryCard);
 $memoryCardFront.insertAdjacentHTML("afterbegin", $iconFront);
+
+/* challenge basic */
+$root.insertBefore($memoryCard2, $memoryCard);
+$memoryCard2.classList.add("memory-card");
+$memoryCard2.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard3, null);
+$memoryCard3.classList.add("memory-card");
+$memoryCard3.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard4, null);
+$memoryCard4.classList.add("memory-card");
+$memoryCard4.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard5, null);
+$memoryCard5.classList.add("memory-card");
+$memoryCard5.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard6, null);
+$memoryCard6.classList.add("memory-card");
+$memoryCard6.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard7, null);
+$memoryCard7.classList.add("memory-card");
+$memoryCard7.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard8, null);
+$memoryCard8.classList.add("memory-card");
+$memoryCard8.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard9, null);
+$memoryCard9.classList.add("memory-card");
+$memoryCard9.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard10, null);
+$memoryCard10.classList.add("memory-card");
+$memoryCard10.insertAdjacentHTML("afterbegin", $iconCollab);
+
+/* ---------------------------------- */
+$root.insertBefore($memoryCard11, null);
+$memoryCard11.classList.add("memory-card");
+$memoryCard11.insertAdjacentHTML("afterbegin", $iconCollab);
