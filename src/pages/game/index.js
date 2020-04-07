2e311d5ca8b20ac94a386fadbefabf2c9a14f896
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-07 20:36:23 -0300

    :art: Add IIFE on pages/game/index.js
    IIFE helps isolate scope the file, is an unnamed function that is invoked after it is declared ()

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,42 +1,44 @@
-const $root = document.querySelector("#root");
+(function () {
+  const $root = document.querySelector("#root");
 
-const $cardsWrapper = createCardsWrapper();
+  const $cardsWrapper = createCardsWrapper();
 
-const createMemoryCard = memoryCard();
-/* createMemoryCard é o return da arrow function do memoryCard */
+  const createMemoryCard = memoryCard();
+  /* createMemoryCard é o return da arrow function do memoryCard */
 
-const $memoryCardBack = createMemoryCard({
-  src: "img/icon-collabcode.png",
-  alt: "Gueio mascote da collabCode"
-});
+  const $memoryCardBack = createMemoryCard({
+    src: "img/icon-collabcode.png",
+    alt: "Gueio mascote da collabCode",
+  });
 
-const $memoryCardFrontC = createMemoryCard({
-  src: "img/icon-c.png",
-  alt: "Icone de um livro da linguagem C++"
-});
+  const $memoryCardFrontC = createMemoryCard({
+    src: "img/icon-c.png",
+    alt: "Icone de um livro da linguagem C++",
+  });
 
-const $memoryCardFrontPHP = createMemoryCard({
-  src: "img/icon-php.png",
-  alt: "Icone de um livro da linguagem PHP"
-});
+  const $memoryCardFrontPHP = createMemoryCard({
+    src: "img/icon-php.png",
+    alt: "Icone de um livro da linguagem PHP",
+  });
 
-const $memoryCardFrontJava = createMemoryCard({
-  src: "img/icon-java.png",
-  alt: "Icone de um livro da linguagem Java"
-});
+  const $memoryCardFrontJava = createMemoryCard({
+    src: "img/icon-java.png",
+    alt: "Icone de um livro da linguagem Java",
+  });
 
-const $memoryCardFrontJs = createMemoryCard({
-  src: "img/icon-js.png",
-  alt: "Icone de um livro da linguagem JavaScript"
-});
+  const $memoryCardFrontJs = createMemoryCard({
+    src: "img/icon-js.png",
+    alt: "Icone de um livro da linguagem JavaScript",
+  });
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontPHP);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontPHP);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontC);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontC);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontPHP);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontPHP);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontC);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontC);
 
-$root.insertAdjacentElement("beforeend", $cardsWrapper);
+  $root.insertAdjacentElement("beforeend", $cardsWrapper);
+})();
