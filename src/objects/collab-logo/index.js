f5e5f2f1bb04674c4dbc0131fbe2bdab9c3753e2
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-05-24 20:37:50 -0300

    :construction: Challenge advanced 039 done

--- src/objects/collab-logo/index.js
+++ src/objects/collab-logo/index.js
@@ -6,9 +6,28 @@ const collabLogo = (function () {
     const $style = document.createElement("style");
 
     $style.textContent = `
-      width: 171px;
-      height: 175px;
-      background-color: #ffffff;
+
+      .container-logo {
+        width: 252px;
+        height: 252px;
+        background-color: #ffffff;
+        position: absolute;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 102px;
+        right: 60px;
+        
+        
+      }
+
+      .collabLogo {
+        width: 171px;
+        height: 175px;
+        background-color: #ffffff;
+      }
+      
 
     `;
     $head.insertAdjacentElement("beforeend", $style);
@@ -18,10 +37,12 @@ const collabLogo = (function () {
     module._style();
 
     return `
-      <img src="img/icon-collabcode.png"
-        alt='O mascote da collabCode'
-        class='collabLogo'
-      />
+      <div class="container-logo">
+        <img src="img/icon-collabcode.png"
+          alt='O mascote da collabCode'
+          class='collabLogo'
+        />
+      </div>
       
     `;
   };
