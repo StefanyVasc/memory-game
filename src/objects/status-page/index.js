6a6ccb397b8214608bc66a97abdf437f3694f947
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-17 19:13:36 -0300

    :construction: status page done
    page 404 works, complete

--- src/objects/status-page/index.js
+++ src/objects/status-page/index.js
@@ -6,10 +6,25 @@ const statusPage = (function () {
     const $style = document.createElement("style");
 
     $style.textContent = `
+      .status-container { 
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        background-color: #eae6da;
+      }
+
       .status-page{
         color: #393f41;
         font-size: 40px;
-        text-transform: uppercase;
+        margin: 0 10px;
+      }
+
+      .icon-status {
+        height: 8vh;
+        margin: 0 10px;
       }
 
       
@@ -18,16 +33,31 @@ const statusPage = (function () {
     $head.insertAdjacentElement("beforeend", $style);
   };
 
-  module.createStatus = (content) => {
+  module.goBack = () => {};
+
+  module.handleClick = (path) => {
+    module.goBack();
+
+    location.hash = `#/${path}`;
+
+    location.reload(true);
+  };
+
+  module.createStatus = ({ content = "", src, alt, path = "" }) => {
     module._style();
     return `
-      <h1 class="status-page">
+      <div class="status-container"> 
+        <h1 class="status-page">
         ${content}
-      </h1>
+        </h1>
+        <input type="image" class="icon-status" src="${src}" alt="${alt}" onClick="statusPage.handleClick('${path}')"/>
+      </div>
+      
     `;
   };
 
   return {
     render: module.createStatus,
+    handleClick: module.handleClick,
   };
 })();
--- src/pages/status404/index.js
+++ src/pages/status404/index.js
@@ -1,7 +1,13 @@
 const page404 = function () {
   const $root = document.querySelector("#root");
 
-  const $status = statusPage.render("error 404 - page not found");
+  const $status = statusPage.render({
+    content:
+      "Ahh, que pena!😞 Não encontramos o que você estava procurando. Tente novamente mais tarde.",
+    src: "img/back.png",
+    alt: "Ícone para voltar a tela",
+    path: "login",
+  });
 
   $root.insertAdjacentHTML("beforeend", $status);
 };
